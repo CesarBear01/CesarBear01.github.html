eacd7377b4a00bc2d7e3c431d4139c364c2f40dc
CesarBear01 <127469823+CesarBear01@users.noreply.github.com>
2023-03-09 23:16:18 -0600

    Add files via upload

---
+++ ADL2.js
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>Aceros del Norte</title>
+    <style>
+      body {
+        background-color: skyblue;
+      }
+      img {
+        width: 30%;
+      }
+    </style>
+  </head>
+  <body>
+    <header>
+      <img src="https://i.imgur.com/bFnHKuf.jpeg" alt="Cerro de la Silla">
+      <h1>Aceros del Norte</h1>
+      <nav>
+        <ul>
+          <li><a href="#">Inicio</a></li>
+          <li><a href="#">Productos</a></li>
+          <li><a href="#">Contacto</a></li>
+        </ul>
+      </nav>
+    </header>
+    <main>
+      <h2>¿Quienes Somos</h2>
+      <p>Somos una empresa dedicada a la comercialización y transforación del Acero, principalmente en rollos de no Primera</p>
+      <h2>Productos que ofrecemos</h2>
+      <ul>
+        <li>PTR</li>
+        <li>Lámina</li>
+        <li>Montén</li>
+      </ul>
+      <h2>Contacto</h2>
+      <p>Si deseas adquirir alguno de nuestros productos o deseas más información sobre ellos, no dudes en contactarnos a través de los siguientes medios:</p>
+      <ul>
+        <li>Teléfono: 771 3169973</li>
+        <li>Dirección: Otumba de Gómez Farías, México</li>
+        <li>Facebook: <a href="https://www.facebook.com/acerosdelnorte/">https://www.facebook.com/acerosdelnorte/</a></li>
+      </ul>
+      <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d15007.912342014985!2d-98.84312085899354!3d19.7058189868119!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x85d1c1"></iframe>
+    </main>
+  </body>
+</html>
+
+
